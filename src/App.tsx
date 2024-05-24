12f4c5e79d7568280ee00ffa0eb7f1f3144f646f
ThankGod <aguthankgod@gmail.com>
2024-05-24 08:56:17 +0100

    new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
 import Services from "./components/Service";
 import Investments from "./components/Investment";
 import TravelInfoComponent from "./components/ExchangeIntro";
 import Security from "./components/Security";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import INDEX from "./components/INDEX";
+import ContactCenter from "./components/Contact";
+import CurrencyRates from "./components/CurrencyRate";
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
-			<Hero />
-			<Services />
-			<Investments />
-			<TravelInfoComponent />
-			<Security />
+			<Routes>
+				<Route path='/' element={<INDEX />} />
+				<Route path='/services' element={<Services />} />
+				<Route path='/investments' element={<Investments />} />
+				<Route path='/travel-info' element={<TravelInfoComponent />} />
+				<Route path='/security' element={<Security />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/contact' element={<ContactCenter />} />
+				<Route path='/rates' element={<CurrencyRates />} />
+			</Routes>
+
 			<Footer />
 		</div>
 	);

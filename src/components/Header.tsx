b76b285cb4d23dcfb469b4f3ed9e2f26b24e0e0d
ThankGod <aguthankgod@gmail.com>
2024-05-24 09:33:43 +0100

    Fix: removing unused data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { IoMdArrowDropup } from "react-icons/io";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
 	const [isConverterDropDownShowing, setisConverterDropDownShowing] = useState<boolean>(false);

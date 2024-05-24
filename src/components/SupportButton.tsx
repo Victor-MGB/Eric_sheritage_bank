247cbe20ac2c6fe46b27d9bea4a7bc5319ede938
ThankGod <aguthankgod@gmail.com>
2024-05-24 20:39:49 +0100

    FEAT: implement support button for both tidio and whatsApp

---
+++ src/components/SupportButton.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+
+const SupportButton = () => {
+	const openWhatsApp = () => {
+		window.open("https://wa.me/yourphonenumber", "_blank");
+	};
+
+	return (
+		<div className='fixed top-[33rem] rounded-full  left-[60rem] animate-bounce shadow-lg shadow-primary  z-50  flex flex-col space-y-2'>
+			<button
+				onClick={openWhatsApp}
+				className='p-4 bg-green-500 text-white rounded-full shadow-lg hover:bg-green-600 transition duration-300 peer'
+			>
+				<FaWhatsapp className={`animate-pulse`} size={24} />
+			</button>
+			<Title />
+		</div>
+	);
+};
+
+export default SupportButton;
+
+const Title = () => {
+	return (
+		<div
+			className={`w-[16rem] peer-hover:flex rounded-xl items-center justify-center gap-2 text-nowrap right-[2rem] absolute bg-black border-[2px] border-green-600 shadow-sm shadow-white h-fit text-[10px] text-Alltext  p-2 hidden`}
+		>
+			chat with assistant via whatsApp <FaWhatsapp className={`text-green-600 animate-pulse`} />
+		</div>
+	);
+};

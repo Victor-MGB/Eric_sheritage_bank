ccb25807fe269ce54ada0b3e6a5c4410f0e32d78
ThankGod <aguthankgod@gmail.com>
2024-06-01 22:32:15 +0100

    Fea: add Amin component

---
+++ src/components/Admin/Admin.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import MainContent from "./Main";
+
+const Admin = () => {
+	return (
+		<div className='flex h-screen'>
+			<Sidebar />
+			<div className='flex-1 p-6 bg-gray-100'>
+				<MainContent />
+			</div>
+		</div>
+	);
+};
+
+export default Admin;

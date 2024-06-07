1ea798922cac26689f418f537b3c701721ccec76
ThankGod <aguthankgod@gmail.com>
2024-06-07 14:49:05 +0100

    modification of style

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -4,7 +4,7 @@ import MainContent from "./Main";
 
 const Admin = () => {
 	return (
-		<div className='flex h-screen'>
+		<div className='flex h-screen '>
 			<Sidebar />
 			<div className='flex-1 p-6 bg-gray-100'>
 				<MainContent />

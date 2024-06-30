b626736d36ca65db5a34acbe27ff49d964453315
TGod <aguthankgod@gmail.com>
2024-06-29 20:37:07 -0700

    feat: new stage components

---
+++ src/components/DashBoard/Stages/Stage_6.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stage_6 = () => {
+  return (
+    <div>stage_6</div>
+  )
+}
+
+export default Stage_6
---
+++ src/components/DashBoard/Stages/Stage_7.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stage_7 = () => {
+  return (
+    <div>Stage_7</div>
+  )
+}
+
+export default Stage_7

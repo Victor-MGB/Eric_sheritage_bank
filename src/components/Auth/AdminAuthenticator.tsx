115b2f51c5669cbd79a38822b95442980181dae3
ThankGod <Aguthankgod@gmail.com>
2024-07-10 09:29:34 +0100

    Feat: new auth warning msg for admin

--- src/components/Auth/AdminAuthenticator.tsx
+++ src/components/Auth/AdminAuthenticator.tsx
@@ -1,16 +1,29 @@
 import { useEffect, useState } from "react";
 import Admin from "../Admin/Admin";
-
+import { Link } from "react-router-dom";
 
 export const AdminAuthenticator = () => {
       const [token, setToken] = useState<string | null>(null);
       useEffect(() => {
             const Token = sessionStorage.getItem("AdminToken");
             setToken(Token);
-      }, [] );
- 
+      }, []);
+
       if (token) {
             return <Admin />;
       }
-      return <div className={`font-bold uppercase text-3xl animate-pulse text-red-500 text-center`}>not authenticated </div>;
+      return (
+            <div className={`font-bold uppercase text-3xl animate-pulse text-red-500 text-center`}>
+                  not authenticated to view this page <br /> <br />
+                  <span>
+                        {" "}
+                        <Link
+                              className={`p-4 border capitalize w-fit shadow-md shadow-black text-black text-xl`}
+                              to={"/sign-in"}
+                        >
+                              login as Admin
+                        </Link>
+                  </span>{" "}
+            </div>
+      );
 };

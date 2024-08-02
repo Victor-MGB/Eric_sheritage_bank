285d9e8451e1b461542384cfa4fb245780535578
Aguthankgod <Aguthankgod@gmail.com>
2024-08-02 13:24:34 +0100

    feat: add translator feature for non-valid user pages

---
+++ src/components/GoogleTranslate.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import './googleStyle.css';
+
+
+declare global {
+  interface Window {
+    googleTranslateElementInit: () => void;
+    google: any;
+  }
+}
+
+const GoogleTranslate: React.FC = () => {
+  useEffect( () => {
+    const scriptId = 'google-translate-script';
+    let scriptAdded = document.getElementById( scriptId );
+
+    const googleTranslateElementInit = () => {
+      try {
+        new window.google.translate.TranslateElement(
+          {
+            pageLanguage: 'en',
+            layout: window.google.translate.TranslateElement.InlineLayout.HORIZONTAL,
+          },
+          'google_translate_element'
+        );
+      } catch ( e ) {
+        console.error( 'Error initializing Google Translate:', e );
+      }
+    };
+
+    // Add the script if not already added
+    if ( !scriptAdded ) {
+      const script = document.createElement( 'script' );
+      script.id = scriptId;
+      script.type = 'text/javascript';
+      script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+      script.onerror = () => {
+        console.error( 'Google Translate script failed to load.' );
+      };
+      document.body.appendChild( script );
+      window.googleTranslateElementInit = googleTranslateElementInit;
+    } else {
+      // If the script is already added, manually call the init function
+      googleTranslateElementInit();
+    }
+
+    // // Cleanup function to remove script and its callback if necessary
+    // return () => {
+    //   if ( scriptAdded ) {
+    //     window.googleTranslateElementInit = undefined;
+    //   }
+    // };
+  }, [] );
+
+  return <div className={ `w-fit` } id="google_translate_element"></div>;
+};
+
+export default GoogleTranslate;

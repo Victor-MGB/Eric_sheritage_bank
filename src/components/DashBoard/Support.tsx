957342c00cbef9c6cab3797a1faa65586d7c9984
Victor MGB <mgbemenaosonduv@gmail.com>
2025-02-18 14:14:28 +0100

    commented

--- src/components/DashBoard/Support.tsx
+++ src/components/DashBoard/Support.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import { FaMapMarkerAlt, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
+import { FaMapMarkerAlt, FaEnvelope} from "react-icons/fa";
 import axios from "axios";
 
 // Define the marker icon
